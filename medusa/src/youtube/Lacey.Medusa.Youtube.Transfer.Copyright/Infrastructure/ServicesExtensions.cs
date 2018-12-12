ac80bda619de334bf26917961ec36c9d0fccf0b2
Lacey Summers <laceybuffysummers@gmail.com>
2018-12-12 15:51:50 +0400

    Medusa. Copyright project.

---
+++ medusa/src/youtube/Lacey.Medusa.Youtube.Transfer.Copyright/Infrastructure/ServicesExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Lacey.Medusa.Youtube.Transfer.Copyright.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lacey.Medusa.Youtube.Transfer.Copyright.Infrastructure
+{
+    public static class ServicesExtensions
+    {
+        public static IServiceCollection AddAppServices(
+            this IServiceCollection services,
+            AppConfiguration config,
+            string connectionString)
+        {
+            return services;
+        }
+    }
+}
---
+++ medusa/src/youtube/Lacey.Medusa.Youtube.Transfer.Copyright/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using AutoMapper;
+using Lacey.Medusa.Youtube.Transfer.Copyright.Configuration;
+using Lacey.Medusa.Youtube.Transfer.Copyright.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Lacey.Medusa.Youtube.Transfer.Copyright
+{
+    class Program
+    {
+        private static ILogger<Program> logger;
+
+        static void Main()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+            var config = configuration.GetSection("App").Get<AppConfiguration>();
+            var connectionString = configuration.GetConnectionString("Default");
+
+            //setup our DI
+            var serviceProvider = new ServiceCollection()
+                .AddAutoMapper()
+                .AddLogging(logBuilder =>
+                    logBuilder
+                        .AddLog4Net()
+                        .SetMinimumLevel(LogLevel.Trace))
+                .AddAppServices(config, connectionString)
+                .BuildServiceProvider();
+
+            //configure console logging
+            logger = serviceProvider.GetService<ILoggerFactory>()
+                .CreateLogger<Program>();
+
+            logger.LogTrace("Welcome to the YouTube copyright tool!");
+
+            serviceProvider.Dispose();
+        }
+    }
+}
